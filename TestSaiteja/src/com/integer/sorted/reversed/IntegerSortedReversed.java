4ab0b1e84ad0d3d3655ff11536118414fe3d13c6
SaitejaSsr <sathirajusaiteja28@gmail.com>
2025-03-20 21:49:13 +0530

    Update IntegerSortedReversed.java

--- TestSaiteja/src/com/integer/sorted/reversed/IntegerSortedReversed.java
+++ TestSaiteja/src/com/integer/sorted/reversed/IntegerSortedReversed.java
@@ -9,19 +9,16 @@ public class IntegerSortedReversed {
 
 	public static void main(String[] args) {
 
-		 // Sorting numbers
         List<Integer> numbers = new ArrayList<>();
         for (int i = 10; i > 0; i--) {
             numbers.add(i);
         }
 
-        // Ascending order sorting
         List<Integer> sortedList = numbers.stream()
                 .sorted()
                 .collect(Collectors.toList());
         System.out.println("Sorted List: " + sortedList);
 
-        // Descending order sorting
         List<Integer> reversedList = sortedList.stream()
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
